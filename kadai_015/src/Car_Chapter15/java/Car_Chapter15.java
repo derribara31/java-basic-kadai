3482fb59a0ff7b97e2471701eb121d3698e459c5
derribara31 <yk31r1236@gmail.com>
2025-01-24 10:43:49 +0900

    Add files via upload

--- kadai_015/src/Car_Chapter15/java/Car_Chapter15.java
+++ kadai_015/src/Car_Chapter15/java/Car_Chapter15.java
@@ -9,22 +9,24 @@ public class Car_Chapter15 {
 		this.speed = speed;
 	}
 	public void gearChange(final int afterGear) {
-		this.gear = afterGear;
-		this.speed = switch(this.gear) {
-		case 1 -> 10;
-		case 2 -> 20;
-		case 3 -> 30;
-		case 4 -> 40;
-		case 5 -> 50;
-		default -> 10;
+		int beforeGear = this.gear;
 		
-		};
+		this.gear = afterGear;
+		switch(afterGear) {
+		case 1 ->this.speed = 10;
+		case 2 -> this.speed =20;
+		case 3 -> this.speed =30;
+		case 4 -> this.speed =40;
+		case 5 -> this.speed =50;
+		default -> this.speed =10;
+		
+		}
+		System.out.println("ギア" + beforeGear + "から" + afterGear + "に切り替えました");
 		
 		
 		
 	}
 	public void run() {
-		System.out.println("ギア1から" + this.gear + "に切り替えました");
 		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 		
